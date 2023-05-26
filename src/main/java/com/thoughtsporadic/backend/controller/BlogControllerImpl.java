9e200f0209cff051512c84792f6fde66835f141c
Aleksandr Smirnov <aa.smirnov2@gmail.com>
2023-05-25 20:30:55 -0600

    refill fake values, fix image urls

--- src/main/java/com/thoughtsporadic/backend/controller/BlogControllerImpl.java
+++ src/main/java/com/thoughtsporadic/backend/controller/BlogControllerImpl.java
@@ -23,25 +23,27 @@ public class BlogControllerImpl implements BlogController {
             100001L,
             "/var/thoughs/death_sentence_there_are_no_good_solutions",
             "Death sentence: there are no good solutions",
-            "https://memphis35-jars.s3.amazonaws.com/death_sentence_there_are_no_good_solutions.png",
+            "https://memphis35-jars.s3.amazonaws.com/images/death_sentence_there_are_no_good_solutions.png",
             """
-                    POST ABOUT DEATH PENALTY
+                    The American Civil Liberties Union believes the death penalty inherently violates the constitutional ban against cruel and unusual punishment and the guarantees of due process of law and of equal protection under the law. Furthermore, we believe that the state should not give itself the right to kill human beings – especially when it kills with premeditation and ceremony, in the name of the law or in the name of its people, and when it does so in an arbitrary and discriminatory fashion.
+                    Capital punishment is an intolerable denial of civil liberties and is inconsistent with the fundamental values of our democratic system.  The death penalty is uncivilized in theory and unfair and inequitable in practice.  Through litigation, legislation, and advocacy against this barbaric and brutal institution, we strive to prevent executions and seek the abolition of capital punishment.
                     """);
 
     private final BlogPost post2 = new FakedBlogPost(100002L,
             "/var/thoughs/what_feminism_means_today",
             "What feminism means today",
-            "https://memphis35-jars.s3.amazonaws.com/what_feminism_means_today.jpg",
+            "https://memphis35-jars.s3.amazonaws.com/images/what_feminism_means_today.jpg",
             """
-                     POST ABOUT FEMINISM
+                    There have been many extraordinary women who have played an important role in local or world history, but not all of these have necessarily been advocates of women’s issues. The women’s movement is made up of women and men who work and fight to achieve gender equality and to improve the lives of women as a social group. In most societies, women were traditionally confined to the home as daughters, wives and mothers, and we are often only aware of women in history because of their relation to famous men. Of course many women throughout history did in fact play an important role in cultural and political life, but they tend to be invisible. An organised women’s movement only really started in the 19th century, even though women activists and the struggle for equality have always been part of all human societies.
                     """);
 
     private final BlogPost post3 = new FakedBlogPost(100003L,
             "/var/thoughs/revenge_cheating_what_you_need_to_know",
             "Revenge Cheating: What You Need to Know",
-            "https://memphis35-jars.s3.amazonaws.com/revenge_cheating.jpg",
+            "https://memphis35-jars.s3.amazonaws.com/images/revenge_cheating.jpg",
             """
-                    POST ABOUT RELATIONSHIPS
+                    Revenge cheating is a type of infidelity committed in retaliation by a person who has been cheated on, in hopes to get revenge on the partner who cheated first. Being cheated on is very painful, so the urge to seek revenge and get justice can help people justify the act of cheating back to teach their partner a lesson.
+                    As with all types of infidelity, revenge cheating can be a physical or emotional affair, and can happen in person or online. Revenge cheating in a person can include one-on-one meet-ups where there is physical contact or intense emotional intimacy that mimics a relationship, whereas online can include messaging an ex, sexting, flirting with others, or sharing explicit photos.
                     """);
 
     @Override
